85213325c820d25d779db3757623a57ce886812e
Matt Post <post@cs.jhu.edu>
2019-01-11 14:19:19 -0500

    added filtering by language ID

--- filtering/filter_by_langid.py
+++ filtering/filter_by_langid.py
@@ -1,19 +1,32 @@
 #!/usr/bin/env python3
 
+import argparse
 import langid
 import sys
 
-_, src, trg = sys.argv
+def main(args):
+    skipped_source = skipped_target = 0
+    for i, line in enumerate(sys.stdin, 1):
+        fields = line.rstrip().split('\t')
+        src_line = fields[args.source_field]
+        trg_line = fields[args.target_field]
+        if langid.classify(src_line)[0] != args.source_lang:
+            skipped_source += 1
+            continue
+        elif langid.classify(trg_line)[0] != args.target_lang:
+            skipped_target += 1
+            continue
+        print(line.rstrip())
 
-skipped_source = skipped_target = 0
-for i, line in enumerate(sys.stdin, 1):
-    src_line, trg_line = line.rstrip().split('\t')
-    if langid.classify(src_line)[0] != src:
-        skipped_source += 1
-        continue
-    elif langid.classify(trg_line)[0] != trg:
-        skipped_target += 1
-        continue
-    print(src_line, trg_line, sep='\t')
+    print('Skipped {} / {} lines (source {} and target {})'.format(skipped_source + skipped_target, i,
+                                                                   skipped_source, skipped_target), 
+          file=sys.stderr)
 
-print('Processed {} lines, skipping {} (source) and {} (target)'.format(i, skipped_source, skipped_target), file=sys.stderr)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('source_lang')
+    parser.add_argument('target_lang')
+    parser.add_argument('--source-field', '-sf', type=int, default=0)
+    parser.add_argument('--target-field', '-tf', type=int, default=1)
+    args = parser.parse_args()
+    main(args)

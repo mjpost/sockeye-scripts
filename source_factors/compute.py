a67408d9a20108b1ace3b365f00edf0c7d11be15
Matt Post <post@cs.jhu.edu>
2019-01-17 15:29:30 -0500

    new JSON-aware source factors scripts

---
+++ source_factors/compute.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python3
+# *-* coding: utf-8 *-*
+
+"""
+Takes tokenized input and computes the subword representation, as well as any requested
+factors, which are broadcast across the subwords.
+"""
+import argparse
+import json
+import sys
+
+from abc import ABC, abstractmethod
+
+from broadcast import broadcast
+from factors import CaseFactor, SubwordFactor
+from typing import Iterable, List, Generator
+
+#from . import factors
+
+class Subwordenizer(ABC):
+    """
+    Generic class for providing subword segmentation.
+    This base class does nothing.
+    """
+    def __init__(self):
+        pass
+
+    def segment(self, line: str) -> str:
+        return line
+
+class BPE(Subwordenizer):
+    def __init__(self, model_path):
+        from subword_nmt import apply_bpe
+        self.model = apply_bpe.BPE(model_path)
+
+    def segment(self, sentence) -> str:
+        pass
+
+
+class SentencePiece(Subwordenizer):
+    def __init__(self, model_path):
+        import sentencepiece as spm
+        self.model = spm.SentencePieceProcessor()
+        self.model.Load(model)
+
+    def segment(self, sentence):
+        pass
+        
+
+def get_subwordenizer(method, model_path):
+    if method == 'bpe':
+        return BPE(model_path)
+    elif method == 'sentencepiece':
+        return SentencePiece(model_path)
+    else:
+        return Subwordenizer()
+
+
+def main(args):
+
+    subwordenizer = None
+
+    factors = []
+    for factor in args.factors:
+        if factor == 'case':
+            factors.append(CaseFactor())
+        elif factor == 'subword':
+            factors.append(SubwordFactor())
+            subwordenizer = get_subwordenizer(args.subword_type, args.subword_model)
+
+    for lineno, line in enumerate(args.input, 1):
+        jobj = json.loads(line)
+
+        if subwordenizer is not None:
+            jobj['subword'] = subwordenizer.segment(jobj['text'])
+            
+        factor_names = args.factors
+        factors = dict(zip(factor_names, [f.compute(jobj) for f in factors]))
+
+        jobj['factor_names'] = factor_names
+        jobj['factors'] = [factors[f] for f in factor_names]
+
+        if subwordenizer is not None:
+            jobj['factors'] = broadcast(factors['subword'], jobj['factors'])
+
+        print(json.dumps(jobj), file=args.output, flush=True)
+
+
+if __name__ == '__main__':
+    params = argparse.ArgumentParser(description='Compute factors over a token stream, then applies optional casing and subword processing.')
+    params.add_argument('--input', '-i',
+                        default=sys.stdin,
+                        type=argparse.FileType('r'),
+                        help='File stream to read tokenized data from. Default: STDIN.')
+    params.add_argument('--output', '-o',
+                        type=argparse.FileType('w'),
+                        default=sys.stdout,
+                        help='Output file to write to. Default: STDOUT.')
+    params.add_argument('factors',
+                        nargs='+',
+                        default=[],
+                        help="List of factors to compute.")
+    params.add_argument('--subword-type', '-s',
+                        choices=['bpe', 'sentencepiece', 'none'],
+                        default='none')
+    params.add_argument('--subword-model', '-m', 
+                        default=None,
+                        type=str,
+                        help='Path to the subword model.')
+    params.add_argument('--json', action='store_true',
+                        help='Work with JSON input and output.')
+    args = params.parse_args()
+
+    main(args)
---
+++ source_factors/factors.py
@@ -0,0 +1,123 @@
+# *-* encoding: utf-8 *-*
+
+from abc import ABC, abstractmethod
+from typing import Dict
+
+class Factor(ABC):
+    @abstractmethod
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def compute(self,
+                jobj: Dict):
+        raise NotImplementedError()
+
+
+class SubwordFactor(Factor):
+    def __init__(self):
+        pass
+
+    def compute_sp(self, sp_str: str) -> str:
+        """
+        iron cement is a ready for use paste which is laid as a fillet by putty knife or finger in the mould edges ( corners ) of the steel ingot mould .
+        ▁iron ▁c ement ▁is ▁a ▁ready ▁for ▁use ▁past e ▁which ▁is ▁laid ▁as ▁a ▁fill et ▁by ▁put ty ▁kni fe ▁or ▁finger ▁in ▁the ▁mould ▁edge s ▁( ▁corner s ▁) ▁of ▁the ▁steel ▁in go t ▁mould ▁.
+
+        The options are:
+        O: a complete word
+        B: beginning of a multi-token word
+        I: interior of a multi-token word
+        E: end of a multi-token word
+        """
+
+        def process_stack(stack):
+            factors_ = []
+            if len(stack) == 1:
+                factors.append('O')
+            elif len(stack) > 1:
+                stack[0] = 'B'
+                # all interior words are 'I'
+                for i in range(1, len(stack) - 1):
+                    stack[i] = 'I'
+                stack[-1] = 'E'
+                factors_ += stack
+            return factors_
+
+        factors = []
+        tokens = sp_str.split()
+        stack = [tokens.pop(0)]
+        for i, token in enumerate(tokens):
+            if token.startswith('▁'):
+                factors += process_stack(stack)
+                stack = []
+            stack.append(token)
+
+        factors += process_stack(stack)
+
+        return ' '.join(factors)
+
+    def compute_bpe(self, bpe_str: str) -> str:
+        """
+        Computes NER-style features for a BPE stream. e.g.,
+
+        The boy ate the waff@@ le .
+          O   O   O   O      B  E O
+
+        The options are:
+        O: a complete word
+        B: beginning of a multi-token word
+        I: interior of a multi-token word
+        E: end of a multi-token word
+
+        :param bpe_str: The BPE string.
+        :return: A string of BPE factors.
+        """
+        factors = []
+        was_in_bpe = False
+        for i, token in enumerate(bpe_str.split()):
+            now_in_bpe = token.endswith('@@')
+            if was_in_bpe:
+                if now_in_bpe:
+                    factor = 'I'
+                else:
+                    factor = 'E'
+            else:
+                if now_in_bpe:
+                    factor = 'B'
+                else:
+                    factor = 'O'
+
+            was_in_bpe = now_in_bpe
+            factors.append(factor)
+
+        return ' '.join(factors)
+
+    def compute(self, jobj: Dict) -> str:
+        """
+        Computes features over a subword string.
+        Automatically determines whether its SentencePiece or BPE.
+        """
+
+        subword_str = jobj['subword']
+
+        return self.compute_sp(subword_str) if '▁' in subword_str else self.compute_bpe(subword_str)
+
+
+class CaseFactor(Factor):
+    def __init__(self):
+        pass
+
+    def case(self, token):
+        if token.isupper():
+            return 'UPPER'
+        elif token.istitle():
+            return 'Title'
+        elif token.islower():
+            return 'lower'
+        else:
+            return '-'
+
+    def compute(self, jobj):
+        line = jobj['text']
+        return ' '.join([self.case(token) for token in line.split()])
+

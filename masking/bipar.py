fdb556f0e5052b8643cc02b5a63168f8121981a2
Matt Post <post@cs.jhu.edu>
2019-06-20 14:05:13 -0400

    reads attention now too

--- masking/bipar.py
+++ masking/bipar.py
@@ -50,7 +50,10 @@ for lineno, line in enumerate(sys.stdin, 1):
     for key, value in target_mask_index_str.items():
         target_mask_str_index.setdefault(value, list()).append(key)
     source_toks = obj["subword_text"].strip().split()
-    attention = -numpy.array(obj["attention"]).transpose()
+    if 'attention' in obj:
+        attention = -numpy.array(obj["attention"]).transpose()
+    else:
+        attention = -numpy.array(obj['alignment']).transpose()
     masks = obj["masks"]
 
     # for each identical mask
